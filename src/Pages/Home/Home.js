f934abb710ea5c257fe060b2904fd89b640b603e
DevMohi <morphex72@gmail.com>
2022-05-10 21:07:20 +0600

    Home page created and made medium responsive

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import Banner from './Banner';
+import Care from './Care';
+import Footer from './Footer';
+import Info from './Info';
+import Login from './Login';
+import MakeAppointment from './MakeAppointment';
+import Service from './Service';
+import Services from './Services';
+import Testimonials from './Testimonials';
+
 
 const Home = () => {
     return (
-        <div>
+        <div className='px-12'>
             <Banner></Banner>
+            <Info ></Info>
+            <Services></Services>
+            <Care></Care>
+            <MakeAppointment></MakeAppointment>
+            <Testimonials></Testimonials>
+            <Login></Login>
+            <Footer></Footer>
         </div>
     );
 };
